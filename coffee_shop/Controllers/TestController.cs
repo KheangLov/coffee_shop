8034ecf8bb371b1aef43a8fccfa3c820508df9a6
Zen168 <sengsothea006@gmail.com>
2019-01-27 11:51:27 +0700

    add action result test

--- coffee_shop/Controllers/TestController.cs
+++ coffee_shop/Controllers/TestController.cs
@@ -13,5 +13,10 @@ namespace coffee_shop.Controllers
         {
             return View();
         }
+
+        public ActionResult Test()
+        {
+            return View();
+        }
     }
 }
