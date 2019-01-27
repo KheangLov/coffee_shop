4221b1e3452f810d4feec73a672404f40ad11905
lovsokheang <lovsokheang@gmail.com>
2019-01-27 10:45:00 +0700

    Add project files.

---
+++ coffee_shop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(coffee_shop.Startup))]
+namespace coffee_shop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
